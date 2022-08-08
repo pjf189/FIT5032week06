70a2cf2ed26dbc201cdec4103158f08f2cd1da43
panjf <panjf@asiainfo.com>
2022-08-08 19:39:29 +0800

    healthDeliverySystem

---
+++ HealthDelievey/HealthDelievey/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using HealthDelievey.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthDelievey.Controllers
+{
+    public class LoginController : Controller
+    {
+        // link to database
+        private Models.HealthDeliveryDBEntities HealthDeliveryDB = new HealthDeliveryDBEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //POST: login implement
+        [HttpPost]
+        public ActionResult Index(String username, String password)
+        {
+            if (String.IsNullOrEmpty(username))
+            {
+                ViewBag.Notice = "Username cannot be empty";
+                return View();
+            }
+            if (String.IsNullOrEmpty(password))
+            {
+                ViewBag.Notice = "Password cannot be empty";
+                return View();
+            }
+            h_admin admin = HealthDeliveryDB.h_admin.FirstOrDefault(p => p.username == username);
+            if(admin == null)
+            {
+                ViewBag.Notice = "User does not exist";
+            }else if(admin.pass != password)
+            {
+                ViewBag.Notice = "Wrong password";
+            }
+            else
+            {
+                return RedirectToAction("/Home/Index");
+            }
+            return View();
+        }
+    }
+}
